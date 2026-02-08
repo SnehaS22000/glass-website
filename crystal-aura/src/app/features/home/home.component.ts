440134107bc75925f06942fc29c6b462139bb151
SnehaS2000 <Snehasoundarrajan4610@gmail.com>
2026-02-08 23:44:08 +0530

    build

--- crystal-aura/src/app/features/home/home.component.ts
+++ crystal-aura/src/app/features/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesSectionComponent } from './sections/categories/categories-section.component';
 import { HeroSectionComponent } from './sections/hero/hero-section.component';
+import { FeaturedCollectionsComponent } from './sections/featured-collections/featured-collections.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent, CategoriesSectionComponent],
+  imports: [CommonModule, HeroSectionComponent, CategoriesSectionComponent,FeaturedCollectionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

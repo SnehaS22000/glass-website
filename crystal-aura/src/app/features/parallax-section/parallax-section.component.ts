38f6482d22e188ee8332e76c9ec90d269bf46900
SnehaS2000 <Snehasoundarrajan4610@gmail.com>
2026-02-11 01:30:07 +0530

    image

--- crystal-aura/src/app/features/parallax-section/parallax-section.component.ts
+++ crystal-aura/src/app/features/parallax-section/parallax-section.component.ts
@@ -1,12 +1,26 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-parallax-section',
-  imports: [],
   templateUrl: './parallax-section.component.html',
-  styleUrl: './parallax-section.component.scss'
+  styleUrls: ['./parallax-section.component.scss']
 })
 export class ParallaxSectionComponent {
 
+  constructor(private elRef: ElementRef) { }
 
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const value = window.scrollY;
+
+    const parallaximage = this.elRef.nativeElement.querySelector('parallax-image');
+    const background = this.elRef.nativeElement.querySelector('background-image');
+
+    if (parallaximage) {
+      parallaximage.style.transform = `translate(${value * 0.2}px, ${value * 0.5}px)`;
+    }
+    if (background) {
+      background.style.transform = `translate(${value * -0.1}px, ${value * 0.3}px)`;
+    }
+  }
 }

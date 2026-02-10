9fedf583cac1b089f3edec54251a6daec08784f5
SnehaS2000 <Snehasoundarrajan4610@gmail.com>
2026-02-10 22:28:58 +0530

    updated

--- crystal-aura/src/app/features/parallax-section/parallax-section.component.ts
+++ crystal-aura/src/app/features/parallax-section/parallax-section.component.ts
@@ -8,27 +8,5 @@ import { Component, HostListener } from '@angular/core';
 })
 export class ParallaxSectionComponent {
 
-//  @HostListener('window:scroll', [])
-// onWindowScroll() {
-//   const ocean = document.getElementById('ocean');
-//   if (!ocean) return;
-
-//   const section = ocean.closest('section') as HTMLElement;
-//   const rect = section.getBoundingClientRect();
-
-//   const viewportHeight = window.innerHeight;
-
-//   // progress: 0 → fully visible, 1 → fully hidden
-//   const progress = Math.min(
-//     Math.max((viewportHeight - rect.top) / rect.height, 0),
-//     1
-//   );
-
-//   // hide from bottom
-//   const hidePercent = progress * 100;
-
-//   ocean.style.clipPath = `inset(0 0 ${hidePercent}% 0)`;
-// }
-
 
 }
